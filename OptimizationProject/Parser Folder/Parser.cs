58d38907b51097746a4d395377b258b57ae1649d
Paweł Jasiński <p.jasinski.2@stud.elka.pw.edu.pl>
2021-04-08 21:48:14 +0200

    eee, średnik xD

--- OptimizationProject/Parser Folder/Parser.cs
+++ OptimizationProject/Parser Folder/Parser.cs
@@ -15,7 +15,7 @@ namespace OptimizationProject.Parser_Folder
         public Parser()
         {
             Files = new List<string>();
-            FullPath = Path.GetFullPath(CONF_FOLDER)
+            FullPath = Path.GetFullPath(CONF_FOLDER);
             Files.AddRange(Directory.GetFiles(FullPath, "*.txt"));
         }
         public List<Graph> ReadConfigFiles()
