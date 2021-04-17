fe4d22970194e410516aa2df9c470bd91f9dc4d9
Paweł Jasiński <p.jasinski.2@stud.elka.pw.edu.pl>
2021-04-17 12:15:27 +0200

    generacja warunków początkowych i testu warunków - DAP

--- OptimizationProject/Algorithm Folder/DAP.cs
+++ OptimizationProject/Algorithm Folder/DAP.cs
@@ -9,31 +9,65 @@ namespace OptimizationProject.Algorithm_Folder
 {
     class DAP
     {
-        private Graph Graph;
+        private Graph graph;
         private StopCondition Condition;
-        private int StartingPopulation;
+        private int ConditionValue;
         private double ProbabilityCrossOver;
         private double ProbabilityMutation;
+        
         Random random;
-        private int ConditionValue;
-
         private List<DAPchromosome> CurrentResultsTable;
+        private int Time;
+        private int NoMutations;
+        private int NoGenerations;
+        private int NoBetterSollutions;//Ilość iteracji bez poprawy
 
         public DAP(Graph graph, StopCondition condition, int startingPopulation, double probabilityCrossOver, double probabilityMutation, int timeGeneratorSeed, int conditionValue)
         {
-            Graph = graph;
+            this.graph = graph;
             Condition = condition;
-            StartingPopulation = startingPopulation;
             ProbabilityCrossOver = probabilityCrossOver;
             ProbabilityMutation = probabilityMutation;
             random = new Random(timeGeneratorSeed);
             ConditionValue = conditionValue;
+
+            for (int i = 0; i < startingPopulation; i++)
+                CurrentResultsTable.Add(new DAPchromosome(graph, random));
+
+            Time = 0;
+            NoMutations = 0;
+            NoGenerations = 0;
+            NoBetterSollutions = 0;
+
         }
 
         public Result run()
         {
+            Result result = new Result();
+            while(checkStopCondition())
+            {
 
-            return null;
+            }
+            return result;
+        }
+        private bool checkStopCondition()
+        {
+            switch(Condition)
+            {
+                case StopCondition.Time:
+                    if (Time > ConditionValue) return false;
+                    break;
+                case StopCondition.LackOfBetterSolution:
+                    if (NoBetterSollutions > ConditionValue) return false;
+                    break;
+                case StopCondition.NoGenerations:
+                    if (NoGenerations > ConditionValue) return false;
+                    break;
+                case StopCondition.NoMutations:
+                    if (NoMutations > ConditionValue) return false;
+                    break;
+            }
+            return true;
         }
     }
 }
